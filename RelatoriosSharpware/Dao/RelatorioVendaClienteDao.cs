402224d1c50f4457605dacb7e9015fa2eff1c6cb
Christopher Nicolas Mauricio <christopher.nicolas.mauricio@gmail.com>
2015-11-22 19:02:21 -0200

    Inicio Repositorio...

---
+++ RelatoriosSharpware/Dao/RelatorioVendaClienteDao.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace RelatoriosSharpware.Dao
+{
+    class RelatorioVendaClienteDao
+    {
+
+        private MySqlConnection _con;
+
+        public RelatorioVendaClienteDao(MySqlConnection con)
+        {
+            _con = con;
+        }
+        public RelatorioVendaCliente relatorioVendaCliente;
+
+        public RelatorioVendaCliente BuscarVenda(int id)
+        {
+            MySqlDataReader reader = null;
+            string cmdText = @"
+                             SELECT
+                             ven.id,
+                             ven.data_venda,
+                             ven.valor_total,
+                             ven.tipo_venda,
+                             cli.nome,
+                             cli.email,
+                             cli.cpf,
+                             fun.nome
+                             FROM venda ven
+                             INNER JOIN cliente cli 
+                             ON cli.id = ven.id_cliente
+                             INNER JOIN funcionario fun
+                             ON fun.id = ven.id_funcionario
+                             WHERE ven.id = @VENDA";
+
+            using (MySqlCommand cmd = new MySqlCommand(cmdText, _con))
+            {
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@VENDA", id);
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    RelatorioVendaCliente relatorioVendaCliente;
+                    while (reader.Read())
+                    {
+                        RelatorioVendaCliente RelVenda = new RelatorioVendaCliente();
+                        RelVenda.IdVenda = reader.GetInt32("vend.id");
+                        RelVenda.DataVenda = reader.GetDateTime("vend.data_venda");
+                        RelVenda.ValorVenda = reader.GetDouble("vend.valor_total");
+                        RelVenda.TipoVenda = reader.GetString("vend.tipo_venda");
+                        RelVenda.NomeCliente = reader.GetString("cli.nome");
+                        RelVenda.EmailCliente = reader.GetString("cli.email");
+                        RelVenda.CpfCliente = reader.GetString("cli.cpf");
+                        RelVenda.NomeFuncionario = reader.GetString("fun.nome");
+                        relatorioVendaCliente = RelVenda;
+                    }
+                }
+            }
+            reader.Close();
+            return relatorioVendaCliente;
+        }
+
+        public void BuscarListaItensProduto(int idVenda)
+        {
+
+        }
+
+    }
+}
---
+++ RelatoriosSharpware/Model/RelatorioVendaClienteModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelatoriosSharpware.Model
+{
+    class RelatorioVendaClienteModel
+    {
+
+        private Int32 parametroIdVenda;
+        private string nomeCliente;
+        private string cpfCliente;
+        private string emailCliente;
+        private Int32 idVenda;
+        private DateTime dataVenda;
+        private Double valorVenda;
+        private string tipoVenda;
+        private string nomeFuncionario;
+
+        public String NomeCliente
+        {
+            get { return nomeCliente; }
+            set { nomeCliente = value; }
+        }
+
+        public String CpfCliente
+        {
+            get { return cpfCliente; }
+            set { cpfCliente = value; }
+        }
+
+        public string EmailCliente
+        {
+            get { return emailCliente; }
+            set { emailCliente = value; }
+        }
+
+        public Int32 IdVenda
+        {
+            get { return idVenda; }
+            set { idVenda = value; }
+        }
+
+        public DateTime DataVenda
+        {
+            get { return dataVenda; }
+            set { dataVenda = value; }
+        }
+
+        public Double ValorVenda
+        {
+            get { return valorVenda; }
+            set { valorVenda = value; }
+        }
+
+        public string TipoVenda
+        {
+            get { return tipoVenda; }
+            set { tipoVenda = value; }
+        }
+
+        public string NomeFuncionario
+        {
+            get { return nomeFuncionario; }
+            set { nomeFuncionario = value; }
+        }
+
+        // Parametro para select .....
+        public Int32 ParametroIdVenda
+        {
+            get { return parametroIdVenda; }
+            set { parametroIdVenda = value; }
+        }
+    }
+}
